8f86d240f7d1b36e84f83cc9573f65cf16e18f36
Shosama16 <roviccuevas11@gmail.com>
2025-02-08 20:38:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
+    "Sure na yan?",
+    "As in?",
+    "Luh, seryoso?",
     "Pookie please...",
     "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
+    "If you say no, iiyak ako...",
+    "ma long coat ako niyan...",
+    "awit parang gagsti..",
+    "Sige na nga, I will stop asking na...",
     "Just kidding, say yes please! ❤️"
 ];
 
