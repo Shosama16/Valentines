2cf8036918fd9ba3dcf1b7e21b9a4738ce38b717
Shosama16 <roviccuevas11@gmail.com>
2025-02-08 20:45:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const messages = [
     "Sure na yan?",
     "As in?",
     "Luh, seryoso?",
-    "Loveee please...",
+    "Loveeeee pleaseee...",
     "Just think about it!",
     "If you say no, iiyak ako...",
     "ma long coat ako niyan...",
